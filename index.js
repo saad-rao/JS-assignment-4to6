b1d93aca1f8a2ea6e4b71fc0085707075ad2b694
Saad_Naseem <167574691+saad-rao@users.noreply.github.com>
2024-08-19 15:54:41 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,405 @@
+                        //CHAPTER 4 ASSIGNMENT
+                       
+                       // 3 variable in one statement
+
+// var x = 3 , y = 2 , z = 1;
+         
+         
+         
+                             //5 LEGAL VARIABLE NAMES
+
+
+//var firstName = saad;      //Starts with a letter and follows camelCase
+
+//var _firstName = saad;     // Starts with an underscore, which is allowed.
+
+//var $first_Name=saad;      //Starts with a dollar sign, which is allowed.
+
+//var first_Name=saad;       //Uses an underscore within the name, which is allowed.
+
+//var firstName123 =saad;   //Contains numbers but does not start with one. 
+
+
+
+
+
+                            //5 ILLEGAL VARIABLE NAMES
+
+// var 123firstname = saad;
+
+// var first name = saad;
+
+// var first-name = saad;
+
+// var #firstname = saad;
+
+// var first - name= saad;
+
+
+                           //DISPLAY RULES IN YOUR BROWSER
+
+                    
+// var rule = 'Rules for naming JS variables <br>';
+// document.write("<b>" , rule ,"</b>") 
+
+
+// var variableNames = "Variable names can only contain Numbers , $ , _ . for example: $first_1stName <br>";
+// document.write(variableNames)
+
+// var variableStart = 'Variable must start with a letter , $ , _. for example: $name , _name or name <br>';
+// document.write(variableStart)
+
+// var caseSenitive = 'Variable names are case sensitive <br>'
+// document.write(caseSenitive)
+
+// var jsKeyword = 'Variable should not be JS keyword';
+// document.write(jsKeyword) 
+
+
+
+
+
+
+                           //CHAPTER 5 ASSIGNMENT
+
+                           //ADD TWO NUMBERS
+
+
+// var num1 = 3;
+// var num2 = 5;
+// var result = num1+num2;
+// document.write("Addition of" + ' ' + num1 + ' ' + "and" +' '+ num2  +' '+ "is" +' '+"<b>", result ,"</b>" , "<br>") 
+
+
+
+// var num1 = 3;
+// var num2 = 5;
+// var result = num1-num2;
+// document.write("subtraction of" + ' ' + num1 + ' ' + "and" +' '+ num2  +' '+ "is" +' '+ "<b>", result ,"</b>" , "<br>")
+
+
+// var num1 = 3;
+// var num2 = 5;
+// var result = num1*num2;
+// document.write("Multiplication of" + ' ' + num1 + ' ' + "and" +' '+ num2  +' '+ "is" +' '+"<b>", result ,"</b>" , "<br>")
+
+
+// var num1 = 3;
+// var num2 = 5;
+// var result = num1/num2;
+// document.write("division of" + ' ' + num1 + ' ' + "and" +' '+ num2  +' '+ "is" +' '+"<b>", result ,"</b>" , "<br>")
+
+
+// var num1 = 3;
+// var num2 = 5;
+// var result = num1%num2;
+// document.write("modulus of" + ' ' + num1 + ' ' + "and" +' '+ num2  +' '+ "is" +' '+ "<b>", result ,"</b>", "<br>")
+                             
+
+
+
+
+
+
+
+                               // QUESTION NO: 3
+
+// var declare = 'Value after variable declaration is ?? <br>';
+// document.write(declare)
+// var number = 5;
+// document.write('Initial value : ', "<b>", number , "</b>" ,"<br>")
+
+// var number = 5
+// document.write("Value after increment is" +' '+ "<b>" ,  ++number ,"</b>" , "<br>")
+
+// var number = number+7;
+// document.write('value after addition of 7 is :' ,  "<b>" , number , "</b>" , "<br>" )
+
+// var number = 13;
+// document.write('value after decrement is : ' +' '+ "<b>" ,--number , "</b>" ,"<br>" )
+
+// var remainder = number/3
+// document.write('The remainder is ' ,"<b>", 0 ,"</b>" )
+
+
+
+
+
+                               //QUESTION NO:4
+                    
+// var ticketPrice = 600;
+// var numberofTickets = 5;
+// var totalCost = ticketPrice*numberofTickets;
+// document.write('Total cost to buy' +' ' + numberofTickets +' '+ 'tickets to a movie is' +' '+ totalCost+''+'PKR' )
+
+
+
+
+
+
+                               //QUESTION NO:5
+
+// var table = "Table of 4";
+// document.write(table ,"<br>" )
+
+// var number = 4;
+// var mutiply = 1;
+// var res = number*mutiply
+// document.write(number ,"x", mutiply   ,"=", res , "<br>")
+
+// var number = 4;
+// var mutiply = 2;
+// var res = number*mutiply
+// document.write(number ,"x", mutiply   ,"=", res , "<br>")
+
+// var number = 4;
+// var mutiply = 3;
+// var res = number*mutiply
+// document.write(number ,"x", mutiply   ,"=", res , "<br>")
+
+// var number = 4;
+// var mutiply = 4;
+// var res = number*mutiply
+// document.write(number ,"x", mutiply   ,"=", res , "<br>")
+
+// var number = 4;
+// var mutiply = 5;
+// var res = number*mutiply
+// document.write(number ,"x", mutiply   ,"=", res , "<br>")
+
+// var number = 4;
+// var mutiply = 6;
+// var res = number*mutiply
+// document.write(number ,"x", mutiply   ,"=", res , "<br>")
+
+// var number = 4;
+// var mutiply = 7;
+// var res = number*mutiply
+// document.write(number ,"x", mutiply   ,"=", res , "<br>")
+
+// var number = 4;
+// var mutiply = 8;
+// var res = number*mutiply
+// document.write(number ,"x", mutiply   ,"=", res , "<br>")
+
+// var number = 4;
+// var mutiply = 9;
+// var res = number*mutiply
+// document.write(number ,"x", mutiply   ,"=", res , "<br>")
+
+// var number = 4;
+// var mutiply = 10;
+// var res = number*mutiply
+// document.write(number ,"x", mutiply   ,"=", res , "<br>")
+
+
+
+
+                                   
+
+
+                                //QUESTION NO:7
+
+
+// var item1Price = 650;
+// document.write('Price of item 1 is' +' '+ item1Price ,"<br>" )
+
+
+// var item1Quantity = 3;
+// document.write('Quantity of item 1 is' +' '+ item1Quantity ,"<br>" )
+
+// var item2Price = 100;
+// document.write('Price of item 2 is' +' '+ item2Price ,"<br>" )
+
+// var item2Quantity = 7;
+// document.write('quantity of item 2 is' +' '+ item2Quantity ,"<br>" )
+
+// var shippingCharges = 100;
+// document.write('Shipping Charges' +' '+ shippingCharges , "<br>")
+
+// var totalCostOfOrder= item1Price*item1Quantity+item2Price*item2Quantity+shippingCharges
+// document.write('Total cost of your order is' +' '+ "<b>" ,totalCostOfOrder ,"</b>") 
+
+
+
+                                  
+
+                                //   CHAPTER 6 MATH EXPRESSION
+
+//QUESTION NO:1 
+
+// document.write("Result" , "<br>")
+// var a = 4;
+// document.write("The value of a is :" +' '+ a , "<br>")
+
+// var a = ++a
+// document.write("The value of ++a is :" +' '+ a , "<br>")
+// document.write("Now the value of a is :" +' '+ a , "<br>")
+
+
+// var a = a++
+// document.write("The value of a++ is :" +' '+ a , "<br>")
+// var a = ++a
+// document.write("Now the value of a is :" +' '+a, "<br>") 
+
+// var a = --a;
+// document.write("The value of --a is :" +' '+ a , "<br>")
+// document.write("Now the value of a is :" +' '+ a , "<br>")
+
+
+// var a = a--;
+// document.write("The value of a-- is :" +' '+ a , "<br>")
+
+// var a = --a
+// document.write("Now the value of a is :" +' '+ a)
+
+
+
+//QUESTION NO:2
+
+// var a = 2;
+// var b = 1;
+// var result = --a - --b + ++b + b--;
+
+//  --a = 1 
+//  --b = 0
+//  ++b = 1
+//  b-- = 1
+//  1 - 0 + 1 + 1 = 3
+// result is = 3 
+
+
+
+//QUESTION NO:3
+
+// var myname = prompt("Enter your Name");
+
+// var greet = alert("wellcome to JS")
+
+// var result = ("greet")
+
+
+
+//QUESTION NO:4
+
+// var input = prompt("Enter a number");
+
+// var num1= [1,2,3,4,5,6,7,8,9,10];
+// var result1 = input*num1[0];
+// document.write(input ,"x" ,num1[0] ,"=", result1 , "<br>")
+
+
+// var num2= [1,2,3,4,5,6,7,8,9,10];
+// var result2 = input*num2[1];
+// document.write(input ,"x" ,num2[1] ,"=", result2 , "<br>")
+
+
+// var num3= [1,2,3,4,5,6,7,8,9,10];
+// var result3 = input*num3[2];
+// document.write(input ,"x" ,num3[2] ,"=", result3 , "<br>")
+
+
+// var num4= [1,2,3,4,5,6,7,8,9,10];
+// var result4 = input*num4[3];
+// document.write(input ,"x" ,num4[3] ,"=", result4 , "<br>")
+
+
+// var num5= [1,2,3,4,5,6,7,8,9,10];
+// var result5 = input*num5[4];
+// document.write(input ,"x" ,num5[4] ,"=", result5 , "<br>")
+
+
+// var num6= [1,2,3,4,5,6,7,8,9,10];
+// var result6 = input*num6[5];
+// document.write(input ,"x" ,num6[5] ,"=", result6 , "<br>")
+
+
+// var num7= [1,2,3,4,5,6,7,8,9,10];
+// var result7 = input*num7[6];
+// document.write(input ,"x" ,num7[6] ,"=", result7 , "<br>")
+
+
+// var num8= [1,2,3,4,5,6,7,8,9,10];
+// var result8 = input*num8[7];
+// document.write(input ,"x" ,num8[7] ,"=", result8 , "<br>")
+
+
+// var num9= [1,2,3,4,5,6,7,8,9,10];
+// var result9 = input*num9[8];
+// document.write(input ,"x" ,num9[8] ,"=", result9 , "<br>")
+
+
+// var num10= [1,2,3,4,5,6,7,8,9,10];
+// var result10 = input*num10[9];
+// document.write(input ,"x" ,num10[9] ,"=", result10 , "<br>")
+
+
+
+//QUESTION NO:6 
+
+// document.write("<b>", "Subject","</b>" , "<br>") 
+
+ 
+// var first = ("English")
+// document.write(first , "<br>")
+
+// var second = ("Math")
+// document.write(second, "<br>")
+
+
+// var third =("Urdu")
+// document.write(third , "<br>")
+
+
+// var totalMarks=("Total marks")
+// document.write("<b>", totalMarks ,"</b>", "<br>")
+
+// var firsttotalMarks = 100;
+// document.write(totalMarks, "<br>")
+
+// var secondtotalMarks = 100;
+// document.write(totalMarks , "<br>")
+
+// var thirdtotalMarks = 100;
+// document.write(totalMarks , "<br>")
+
+
+
+// var name1 = prompt("Enter your name")
+
+
+// var subject1= +prompt('Enter your math subject number')
+// var subject2=+prompt('Enter your  english subject number')
+// var subject3=+prompt('Enter your urdu subject number')
+// var subject4=+prompt('Enter your islamiat subject number')
+// var subject5=+prompt('Enter your physics subject number')
+
+// var total = (subject1+subject2+subject3+subject4+subject5)
+
+// var percentage =  (total/500*100)
+
+
+// if (percentage <=80){
+//     document.write( "<b>", name1, "</b>" +' '+  "You got A Grade and your percentage is" +' '+ (percentage))
+// }
+
+// else if (percentage <101){
+//     document.write("<b>", name1, "</b>" +' '+ "you got A+ Grade and your percentage is" +' '+ (percentage))
+// }
+
+// else if (percentage <=70){
+//     document.write("<b>", name1, "</b>" +' '+ "you got A Grade and your percentage is" +' '+ (percentage))
+// }
+// else if (percentage <=60){
+//     document.write("<b>", name1, "</b>" +' '+ "you got B Grade and your percentage is" +' '+ (percentage))
+// }
+// else if (percentage <=50){
+//     document.write("<b>", name1, "</b>" +' '+ "you got C Grade and your percentage is" +' '+ (percentage))
+// }
+// else if (percentage <=40){
+//     document.write("<b>", name1, "</b>" +' '+ "you got D Grade and your percentage is" +' '+ (percentage))
+// }
+
+// else {
+//     document.write("<b>", name1, "</b>" +' '+ "Better luck for next time")
+// }
